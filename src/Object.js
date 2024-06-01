ceb8b22287cfc15b390e11d29ee1125db3f88545
Dmitry <40324321+inilim@users.noreply.github.com>
2024-06-01 19:27:25 +0300

    Update Object.js

--- src/Object.js
+++ src/Object.js
@@ -1,3 +1,12 @@
 export function isObject(value) {
     return typeof value === "object" && !Array.isArray(value) && value !== null;
 }
+
+/**
+ * 
+ * @param {onject} obj
+ * @returns {bool}
+ */
+export function isEmptyObject(obj) {
+    return Object.keys(obj).length === 0;
+}
