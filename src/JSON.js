52dcae469c6a57d42ca9aa68622b57c262086273
Dmitry <40324321+inilim@users.noreply.github.com>
2024-06-01 13:04:30 +0300

    ref

---
+++ src/JSON.js
@@ -0,0 +1,10 @@
+export function isJSON(str) {
+    if (typeof str !== "string") return false;
+    try {
+        const result = JSON.parse(str);
+        const type = Object.prototype.toString.call(result);
+        return type === "[object Object]" || type === "[object Array]";
+    } catch (err) {
+        return false;
+    }
+}
